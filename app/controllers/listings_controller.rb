69d42024c173e899a727db7330bbed8e1ca648e4
Afeez Adekunle <olopadeafeez@gmail.com>
2015-11-25 18:57:12 +0100

    Redesign Seller page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,16 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
   before_action :check_user, only: [:edit, :update, :destroy]
 
 
   # GET /listings
   # GET /listings.json
+  def seller
+    @listings = Listing.where(user: current_user).order("created_at DESC")
+  end
   def index
-    @listings = Listing.all
+    @listings = Listing.all.order("created_at DESC")
   end
 
   # GET /listings/1
@@ -76,6 +79,7 @@ class ListingsController < ApplicationController
         redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
       end
     end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
